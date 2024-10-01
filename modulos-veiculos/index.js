715778e2d493c84c6bf5763c4d14feae7f102373
manuellafagundes <manuellamichetti@gmail.com>
2024-10-01 08:25:05 -0300

    Modulo-Veículos

---
+++ modulos-veiculos/index.js
@@ -0,0 +1,8 @@
+import {acelerar} from "./lib/veiculo.js";
+
+import {fs} from "./lib/modulo.cjs";
+
+console.log(acelerar());
+console.log(acelerar());
+console.log(acelerar());
+console.log(fs);
---
+++ modulos-veiculos/lib/veiculo.js
@@ -0,0 +1,8 @@
+let km = 0
+
+function acelerar(){
+  km = km + 26
+  return `no momento, a velocidade é de ${km}km/h`
+  
+}
+export {acelerar};
